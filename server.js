83671c9879d476473aa60f54d72c6c899fe0bc41
imashish1705 <imashish1705@gmail.com>
2021-03-29 17:48:59 +0530

    auth,login,register,cart_section

--- server.js
+++ server.js
@@ -6,15 +6,30 @@ const expresslayout = require("express-ejs-layouts");
 
 const PORT = process.env.PORT || 3300;
 
-// set templating 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname,"/views"));
- 
 // set assets 
 app.use("/", express.static(path.join(__dirname, "/public")));
 
+
+// set templating 
+app.use(expresslayout);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,"/views"));
+ 
+
 app.get("/",(req,res) => {
     res.render("home");
+})
+
+app.get("/cart", (req,res) => {
+    res.render("customers/cart");
+})
+
+app.get("/login", (req,res) => {
+    res.render("auth/login");
+})
+
+app.get("/register", (req,res) => {
+    res.render("auth/register");
 })
 
 app.listen(PORT, () => {
